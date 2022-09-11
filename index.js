2866d2933e9d48bc6e46f30ba6e2ec4077f12375
HungTranXD <hungtranxd2697@gmail.com>
2022-09-11 15:32:49 +0700

    e-project

--- index.js
+++ index.js
@@ -236,7 +236,7 @@ app.get("/api-bridge-detail", function (req, res){
     conn.query(sql_txt, function (err, data){
         if(err) res.send("Error");
         else {
-            data = data.replace(/\\"/g, "\"");
+            // data = data.replace(/\\"/g, "\"");
             res.send(data);
         };
     })
